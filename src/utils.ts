73857ff9c9628b87bfc89bc5b9bcb77504b3cca2
linhbkhn95 <trinhducbaolinh@gmail.com>
2022-09-05 09:59:44 +0700

    add utils

--- src/utils.ts
+++ src/utils.ts
@@ -17,6 +17,17 @@ export const WethMap: { [network: number]: Address } = {
   250: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
 };
 
+export const WethMap: { [network: number]: Address } = {
+  1: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  3: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+  4: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+  42: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
+  56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+  137: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+  43114: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
+  250: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
+};
+
 export const isETHAddress = (address: string) =>
   address.toLowerCase() === ETHER_ADDRESS.toLowerCase();
 
